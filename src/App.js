baba3568c20aa5a9bb7a225d26e801f99430facc
sophxmia <maliarenko.sofiia@gmail.com>
2023-11-13 17:30:18 +0200

    Added the basic structure to my React-program

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        Weather App
+        <footer>
+          Open-sourse project,{" "}
+          <a
+            href="https://github.com/sophxmia/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+            title="GitHub link"
+          >
+            hosted on GitHub
+          </a>
+          , written by{" "}
+          <a
+            href="https://brilliant-meerkat-d49135.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Sofiia Maliarenko
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
