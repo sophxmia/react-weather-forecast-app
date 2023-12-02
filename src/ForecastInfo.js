1033b85cbb3ba58586af087065c5deb77561ff16
sophxmia <maliarenko.sofiia@gmail.com>
2023-12-02 17:13:33 +0200

    Update ForecastInfo.js
    Added useEffect to solve the bug with updating of the forecast with each change of coordinates

--- src/ForecastInfo.js
+++ src/ForecastInfo.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./ForecastInfo.css";
 import WeatherForecastDay from "./WeatherForecastDay";
 import axios from "axios";
@@ -6,6 +6,11 @@ import axios from "axios";
 export default function ForecastInfo(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
   function handleResponse(response) {
     setForecast(response.data.daily);
     setLoaded(true);
